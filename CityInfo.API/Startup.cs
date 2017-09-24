a3b09bd4071434a874822a1942747c89a997cfa9
yadac <doruji1982@gmail.com>
2017-09-24 19:29:32 +0900

    3-13 serialize - deserialize

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -17,6 +18,16 @@ namespace CityInfo.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            //services.AddMvc()
+            //    .AddJsonOptions(o =>
+            //    {
+            //        if (o.SerializerSettings.ContractResolver != null)
+            //        {
+            //            var castedResolver = o.SerializerSettings.ContractResolver
+            //                as DefaultContractResolver;
+            //            castedResolver.NamingStrategy = null;
+            //        }
+            //    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
